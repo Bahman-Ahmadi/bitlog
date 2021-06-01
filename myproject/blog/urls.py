6a1b80bccb18a48eb0d3142463c17be941fbb195
Bahman <75687940+Bahman-Ahmadi@users.noreply.github.com>
2021-06-01 11:29:22 +0430

    Delete myproject directory

--- myproject/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, re_path
-from blog.views import *
-
-app_name = "blog"
-urlpatterns = [
-    path("",main,name="index"),
-    path("post/<slug:slug>", page, name="page")
-]
